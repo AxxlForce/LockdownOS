1ff34194bb57ba65c2d55beeac73774a868039e1
alex <alexander_stoecker@web.de>
2020-07-04 00:58:04 +0200

    added plank dock items

--- install_applications.sh
+++ install_applications.sh
@@ -24,17 +24,20 @@ apt-get autoremove -y ubuntu-session fwupd gnome-control-center
 # install packaged apps
 apt-get install -y vlc chromium-browser thunderbird gimp pinta ulauncher gnome-games adapta-gtk-theme paper-icon-theme
 
-# install all non packaged stuff
+# install Visual Studio and refine launcher
 wget https://packagecloud.io/headmelted/codebuilds/gpgkey
 apt-key add gpgkey
 wget https://code.headmelted.com/installers/apt.sh
 sh apt.sh
-
-# refine Visual Studio Code launcher
 sed -i 's/\(Name=\)\(.*\)/\1Visual Studio Code/' /usr/share/applications/code-oss.desktop
 sed -i 's/\(Icon=\)\(.*\)/\1\/usr\/share\/icons\/Paper\/512x512\/apps\/visual-studio-code.png/' /usr/share/applications/code-oss.desktop
 
-# install Reaper DAW
-wget https://www.reaper.fm/files/6.x/reaper612c_linux_armv7l.tar.xz -P ~/Downloads/ && cd ~/Downloads
-tar -xvf reaper612c_linux_armv7l.tar.xz
-sudo mv reaper_linux_armv7l/REAPER/ /opt/reaper && rm -rf reaper_linux_armv7l
+# install Reaper DAW and create launcher
+wget https://www.reaper.fm/files/6.x/reaper612c_linux_armv7l.tar.xz -P $HOME/Downloads
+tar -xvf $HOME/Downloads/reaper612c_linux_armv7l.tar.xz -C $HOME/Downloads
+sudo mv $HOME/Downloads/reaper_linux_armv7l/REAPER/ /opt/reaper && rm -rf $HOME/Downloads/reaper_linux_armv7l
+sudo cp ./resources/reaper.desktop /usr/share/applications/
+
+# configure plank launcher
+rm $HOME/.config/plank/dock1/launchers/*
+cp ./resources/*.dockitem $HOME/.config/plank/dock1/launchers
